ed395162f1e6bb3c9ce9e62d4c020e7975175609
Nabin Purja <nabinpurja93@gmail.com>
2021-06-23 14:35:36 +0100

    added the basic logics for checking the player's hand

--- src/components/Blackjack.js
+++ src/components/Blackjack.js
@@ -1,25 +1,70 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from '@chakra-ui/react';
+import { Stack, Button } from '@chakra-ui/react';
 import Deck from './Deck';
+import _ from 'lodash';
 
 const Blackjack = () => {
   const [cards, setCards] = useState([]);
   const [playerHand, setPlayerHand] = useState([]);
+  const [currentTotal, setCurrentTotal] = useState(0);
+  const [outcome, setOutcome] = useState(null);
 
   useEffect(() => {
     const newDeck = new Deck();
     newDeck.createDeck();
     setCards(newDeck.cards);
 
-    console.info('newdeck', newDeck);
+    console.log('newdeck', newDeck);
   }, []);
 
+  useEffect(() => {
+    // starts new cards
+    if (cards.length === 52 && currentTotal === 0) {
+      setOutcome(null);
+      hitCard();
+    }
+  }, [cards]);
+
   const handleHit = () => {
-    setPlayerHand([...playerHand, cards.splice(0, 1)[0]]);
+    hitCard();
   };
 
-  const getHandTotal = () => {
-    return playerHand.reduce((acc, obj) => acc + obj.value, 0);
+  const hitCard = () => {
+    const cardsCopy = [...cards];
+    const newCard = cardsCopy.splice(0, 1)[0];
+    setPlayerHand([...playerHand, newCard]);
+    setCards(cardsCopy);
+
+    let { rank, value } = newCard;
+    // Ace to represent 11 if the sum exceeds 21
+    if (rank === 'A' && currentTotal <= 10) {
+      value = 11;
+    }
+
+    setCurrentTotal(currentTotal + value);
+  };
+
+  // check score
+  useEffect(() => {
+    if (currentTotal === 21) {
+      console.log('score updated --- BLACKJACK');
+      setOutcome('Blackjack');
+    } else if (currentTotal > 21) {
+      console.log('score updated --- BUSTED');
+      setOutcome('Busted');
+    }
+  }, [currentTotal]);
+
+  const handleNewGame = () => {
+    // move the cards on hand to the back.
+    const cardsToGoBack = _.shuffle(playerHand);
+    setCards([...cards, ...cardsToGoBack]);
+    setPlayerHand([]);
+    setCurrentTotal(0);
+  };
+
+  const handleStick = () => {
+    setOutcome('Stick');
   };
 
   return (
@@ -34,11 +79,32 @@ const Blackjack = () => {
         })}
       </div>
 
-      <div className="total-value">{getHandTotal()}</div>
+      <div className="total-value">{currentTotal}</div>
 
-      <Button colorScheme="purple" onClick={() => handleHit()}>
-        Hit
-      </Button>
+      <Stack spacing={4} direction="row" justify="center">
+        <Button
+          colorScheme="purple"
+          onClick={() => handleNewGame()}
+          disabled={!outcome}
+        >
+          New
+        </Button>
+        <Button
+          colorScheme="purple"
+          onClick={() => handleHit()}
+          disabled={outcome}
+        >
+          Hit
+        </Button>
+        <Button
+          colorScheme="purple"
+          variant="outline"
+          onClick={() => handleStick()}
+          disabled={outcome}
+        >
+          Stick
+        </Button>
+      </Stack>
     </div>
   );
 };
